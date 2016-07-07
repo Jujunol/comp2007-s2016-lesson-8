8cd4ebe624b4f94e36f252128e358720bbd59eb5
Jujunol <jujunol@outlook.com>
2016-07-07 14:39:45 -0400

    First commit

---
+++ comp2006-s2016-lesson-8/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace comp2006_s2016_lesson_8
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ comp2006-s2016-lesson-8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp2006_s2016_lesson_8.MvcApplication" Language="C#" %>

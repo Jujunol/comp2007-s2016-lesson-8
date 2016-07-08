b803326dd43450aa7fa709cb0b5f360af7fff0b0
Jujunol <jujunol@outlook.com>
2016-07-08 00:11:05 -0400

    Lab 4 submission

--- comp2006-s2016-lesson-8/App_Start/BundleConfig.cs
+++ comp2006-s2016-lesson-8/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace comp2006_s2016_lesson_8
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
